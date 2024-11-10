30b72afc223a280b9035cd9323926b4d3984ae26
vaishnavi jha <vaishnavijha03aug@gmail.com>
2024-11-10 06:46:43 -0800

    add files

--- controllers/users.js
+++ controllers/users.js
@@ -14,7 +14,7 @@ module.exports.signup = async(req, res)=>{
         if(err){
            return next(err);
         }
-        req.flash("success", "Welcome to WanderLust!");
+        req.flash("success", "Welcome to AirBnb!");
         res.redirect("/listings");
     });
     }catch(e){
@@ -29,7 +29,7 @@ module.exports.renderLoginForm = (req, res)=>{
 };
 
 module.exports.login = async(req, res)=>{
-    req.flash("success", "welcome back to Wanderlust!");
+    req.flash("success", "welcome back to AirBnb!");
     let redirectUrl = res.locals.redirectUrl || "/listings";
     res.redirect(redirectUrl);
 };
